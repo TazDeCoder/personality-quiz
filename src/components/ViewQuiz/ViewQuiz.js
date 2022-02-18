07c1a2c3f07ad7698639558300c05562200c89b3
tarwat-uddin <t.uddin9121@gmail.com>
2022-02-18 18:01:14 +0000

    Add ViewQuiz.js component

---
+++ src/components/ViewQuiz/ViewQuiz.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import Button from "../UI/Button";
+import styles from "./ViewQuiz.module.css";
+
+function ViewQuiz(props) {
+  const [isQuizDraft, setIsQuizDraft] = useState(
+    props.quiz.questions.length !== 0
+  );
+
+  return (
+    <div className={styles["view-quiz"]}>
+      <div className={styles["view-quiz__content"]}>
+        <h1>{props.quiz.title}</h1>
+        <p>{props.quiz.desc}</p>
+      </div>
+
+      <div className={styles["view-quiz__meta"]}>
+        <p>
+          Number of questions: <span>{props.quiz.questions.length}</span>
+        </p>
+        <p>
+          Created By: <span>{props.quiz.author}</span>
+        </p>
+      </div>
+
+      <div className={styles["view-quiz__actions"]}>
+        <Button onClick={props.onEditQuiz}>Edit Quiz</Button>
+        <Button disabled={!isQuizDraft}>Start Quiz</Button>
+      </div>
+    </div>
+  );
+}
+
+export default ViewQuiz;
