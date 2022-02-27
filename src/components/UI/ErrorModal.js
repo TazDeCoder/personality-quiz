b4cb24d73b53e280c395482cf1601c84e192ba16
tarwat-uddin <t.uddin9121@gmail.com>
2022-02-27 15:32:11 +0000

    Add ErrorModal.js component

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,49 @@
+import { createPortal } from "react-dom";
+
+import styles from "./ErrorModal.module.css";
+import Card from "./Card";
+import Button from "./Button";
+
+function Backdrop(props) {
+  return <div className={styles.backdrop} onClick={props.onClose}></div>;
+}
+
+function ModalOverlay(props) {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Ok</Button>
+      </footer>
+    </Card>
+  );
+}
+
+function ErrorModal(props) {
+  const portalEl = document.getElementById("overlays");
+
+  return (
+    <>
+      {createPortal(<Backdrop onClose={props.onClose} />, portalEl)}
+      {createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        >
+          {props.children}
+        </ModalOverlay>,
+        portalEl
+      )}
+    </>
+  );
+}
+
+export default ErrorModal;
