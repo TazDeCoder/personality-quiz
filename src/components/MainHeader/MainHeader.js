462c25d4c3b3e367a9668b3e137c50973dc67159
tarwat-uddin <t.uddin9121@gmail.com>
2022-03-05 11:51:10 +0000

    Improve MainHeader.js component ui
    Minor improvement: displays current quiz title when doing a quiz

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+
 import styles from "./MainHeader.module.css";
 import Navigation from "./Navigation";
+import QuizContext from "../../store/quiz-context";
 
 const MainHeader = (props) => {
+  const quizCtx = useContext(QuizContext);
+
   return (
     <header className={styles["main-header"]}>
-      <h1>Personality Quiz</h1>
+      {!props.startQuiz && <h1>Personality Quiz</h1>}
+      {props.startQuiz && <h1>{quizCtx.title}</h1>}
       <Navigation
         startQuiz={props.startQuiz}
         onToggleForm={props.onToggleQuizForm}
