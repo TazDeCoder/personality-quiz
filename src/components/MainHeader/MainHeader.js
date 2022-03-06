beb6a5559bcaa28f2c52277ccb5b0b5e57fcb129
tarwat-uddin <t.uddin9121@gmail.com>
2022-03-06 18:59:12 +0000

    Improve MainHeader.js component ui
    Major improvements: cleaner JSX code and added 'login' and 'sign-up' buttons

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -5,19 +5,28 @@ import styles from "./MainHeader.module.css";
 import Navigation from "./Navigation";
 // CONTEXTS
 import QuizContext from "../../store/quiz-context";
+import UserContext from "../../store/user-context";
 
 const MainHeader = (props) => {
+  const userCtx = useContext(UserContext);
   const quizCtx = useContext(QuizContext);
 
   return (
     <header className={styles["main-header"]}>
-      {!props.startQuiz && <h1>Personality Quiz</h1>}
+      {!props.startQuiz && <h1>Home</h1>}
       {props.startQuiz && <h1>{quizCtx.title}</h1>}
       <Navigation
         startQuiz={props.startQuiz}
-        onToggleForm={props.onToggleQuizForm}
+        onToggleLoginForm={props.onToggleLoginForm}
+        onToggleSignupForm={props.onToggleSignupForm}
+        onToggleQuizForm={props.onToggleQuizForm}
         onClose={props.onClose}
       />
+      {userCtx.isLoggedIn && !props.startQuiz && (
+        <p>
+          Logged In: <span>{userCtx.username}</span>
+        </p>
+      )}
     </header>
   );
 };
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,20 +1,55 @@
+import { useContext } from "react";
+
 import styles from "./Navigation.module.css";
 // COMPONENTS
 import Button from "../UI/Button/Button";
+// CONTEXTS
+import UserContext from "../../store/user-context";
 
 function Navigation(props) {
+  let listContent;
+
+  const userCtx = useContext(UserContext);
+
+  if (!userCtx.isLoggedIn) {
+    listContent = (
+      <>
+        <li>
+          <Button onClick={props.onToggleLoginForm}>Login</Button>
+        </li>
+
+        <li>
+          <Button onClick={props.onToggleSignupForm}>Sign up</Button>
+        </li>
+      </>
+    );
+  }
+
+  if (userCtx.isLoggedIn && !props.startQuiz) {
+    listContent = (
+      <>
+        <li>
+          <Button onClick={props.onToggleQuizForm}>Add New Quiz</Button>
+        </li>
+
+        <li>
+          <Button onClick={userCtx.onLogout}>Log out</Button>
+        </li>
+      </>
+    );
+  }
+
+  if (props.startQuiz) {
+    listContent = (
+      <li>
+        <Button onClick={props.onClose}>Close Quiz</Button>
+      </li>
+    );
+  }
+
   return (
     <nav className={styles.nav}>
-      <ul>
-        <li>
-          {!props.startQuiz && (
-            <Button onClick={props.onToggleForm}>Add New Quiz</Button>
-          )}
-          {props.startQuiz && (
-            <Button onClick={props.onClose}>Close Quiz</Button>
-          )}
-        </li>
-      </ul>
+      <ul>{listContent}</ul>
     </nav>
   );
 }
